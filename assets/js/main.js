1f318728e2efcb861b4e1a1987a82989bcdccc85
chibuikemokoli <chibuikemokoli@yahoo.com>
2024-10-23 23:07:46 +0100

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,60 @@ const showMenu = (toggleId, navId) => {
 
   toggle.addEventListener('click', () => {
     nav.classList.toggle('show-menu')
-
     toggle.classList.toggle('show-icon')
   })
 }
 
 showMenu('nav-toggle', 'nav-menu')
+
+const dropdownItems = document.querySelectorAll('.dropdown__item')
+
+dropdownItems.forEach((item) => {
+  const dropdownButton = item.querySelector('.dropdown__button')
+
+  dropdownButton.addEventListener('click', () => {
+    
+    const showDropdown = document.querySelector('.show-dropdown')
+
+    toggleItem(item)
+
+    if (showDropdown && showDropdown !== item) {
+      toggleItem(showDropdown)
+    }
+  })
+})
+
+const toggleItem = (item) => {
+  
+  const dropdownContainer = item.querySelector('.dropdown__container')
+
+  if (item.classList.contains('show-dropdown')) {
+    dropdownContainer.removeAttribute('style')
+    item.classList.remove('show-dropdown')
+  } else {
+    dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
+    item.classList.add('show-dropdown')
+  }
+}
+
+const mediaQuery = matchMedia('(min-width: 1118px)'),
+  dropdownContainer = document.querySelectorAll('.dropdown__container')
+
+const removeStyle = () => {
+  
+  if (mediaQuery.matches) {
+    
+    dropdownContainer.forEach((e) => {
+      e.removeAttribute('style')
+    })
+
+    dropdownItems.forEach((e) => {
+      e.classList.remove('show-dropdown')
+    })
+  }
+}
+
+addEventListener('resize', removeStyle)
 
 function scrollTop() {
   const scrollTop = document.getElementById('scroll-top')
@@ -17,17 +65,6 @@ function scrollTop() {
   else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop);
-
-var className = "inverted";
-var scrollTrigger = 170;
-
-window.onscroll = function () {
-  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
-    document.getElementsByTagName("header")[0].classList.add(className);
-  } else {
-    document.getElementsByTagName("header")[0].classList.remove(className);
-  }
-};
 
 const q = document.querySelectorAll('.q');
 const a = document.querySelectorAll('.a');
